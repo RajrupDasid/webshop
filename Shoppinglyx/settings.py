1ead86512552eb8630161d9e6639b61287756724
Rajrup Das <rajrupdasid@outlook.com>
2021-06-21 19:05:31 +0530

    tweaked settings for deployment

--- Shoppinglyx/settings.py
+++ Shoppinglyx/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Shoppinglyx.urls'
@@ -133,8 +137,13 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 LOGIN_REDIRECT_URL='/profile/'
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 SESSION_ENGINE="django.contrib.sessions.backends.db"
+STATICFILES_STORAGE='whitenoise.storage.CompressedManidestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+django_heroku.settings(locals())
